bd3d07bc1406646b32bef37b3b8a1d5026be94b5
Neelesh Shetty <an.neelesh@novasemita.org>
2020-07-03 01:20:21 +0530

    added navbar

---
+++ FRONTEND/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Navigation from './Navbar'
+
+class App extends React.Component {
+    render() {
+        return (
+                <Navigation/>
+        )
+    }
+}
+
+export default App;
