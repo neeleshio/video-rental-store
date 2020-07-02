ae3c3469873fcac44be10b3cb7d411438b7b765a
Neelesh Shetty <an.neelesh@novasemita.org>
2020-07-03 01:21:57 +0530

    added navbar

--- FRONTEND/src/Navbar.js
+++ FRONTEND/src/Navbar.js
@@ -4,13 +4,13 @@ import { Navbar, Nav } from 'react-bootstrap'
 class Navigation extends React.Component {
     render() {
         return (
-                <Navbar bg="dark" variant="dark" style={{'backgroundColor':'#0000'}}>
-                    <Navbar.Brand href="#home" ><span style={{'color': '#ADFF2F'}}>V</span>ideo Rental Store</Navbar.Brand>
-                        <Nav className="mr-auto"></Nav>
-                    <Nav>
-                         <Nav.Link eventKey={2} href="#admin">Admin</Nav.Link>
-                    </Nav>
-                </Navbar>
+            <Navbar bg="dark" variant="dark">
+                <Navbar.Brand href="#home" ><span style={{ 'color': '#ADFF2F' }}>V</span>ideo Rental Store</Navbar.Brand>
+                <Nav className="mr-auto"></Nav>
+                <Nav>
+                    <Nav.Link eventKey={2} href="#admin">Admin</Nav.Link>
+                </Nav>
+            </Navbar>
         )
     }
 }
